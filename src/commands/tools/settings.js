88560a96d4473dfa58f352aba93714a26a9f07d8
muffafa <mm.savar@gmail.com>
2023-04-30 00:00:59 +0300

    feat: ayalar komutuna embed ve butonları eklendi

--- src/commands/tools/settings.js
+++ src/commands/tools/settings.js
@@ -3,6 +3,8 @@ const {
   ButtonBuilder,
   ActionRowBuilder,
   ButtonStyle,
+  EmbedBuilder,
+  Colors,
 } = require("discord.js");
 
 module.exports = {
@@ -10,10 +12,28 @@ module.exports = {
     .setName("ayarlar")
     .setDescription("Ayarlar menüsünü açar."),
 
-  async execute(interaction) {
+  async execute(interaction, client) {
+    const embed = new EmbedBuilder()
+      .setColor(Colors.Blue)
+      .setImage(client.user.displayAvatarURL()) // TODO: Resim figmadaki resimle değiştirilecek
+      .setThumbnail(interaction.user.displayAvatarURL())
+      .setAuthor({
+        url: "https://github.com/Kodluyoruz/discord-register-bot",
+        iconURL: client.user.displayAvatarURL(),
+        name: `Kodluyoruz Kayıt Botu`,
+      })
+      .setURL("https://github.com/Kodluyoruz/discord-register-bot")
+      .addFields([
+        {
+          name: `Kodluyoruz Kayıt Botu Yardım Menüsüne`,
+          value: `Aşağıdaki butonları kullanarak kuruluma başlamdan önce dökümanı okuyabilir ve kuruluma başlayabilirsiniz.`,
+          inline: false,
+        },
+      ]);
+
     const docsButton = new ButtonBuilder()
       .setLabel("Dökümantasyon")
-      .setURL('https://github.com/Kodluyoruz/discord-register-bot')
+      .setURL("https://github.com/Kodluyoruz/discord-register-bot")
       .setStyle(ButtonStyle.Link);
 
     const setRoleButton = new ButtonBuilder()
@@ -22,12 +42,19 @@ module.exports = {
       .setStyle(ButtonStyle.Success);
 
     const setChannelButton = new ButtonBuilder()
-    .setCustomId("setChannel")
-    .setLabel("Ayarları Düzenle")
-    .setStyle(ButtonStyle.Secondary);
+      .setCustomId("setChannel")
+      .setLabel("Ayarları Düzenle")
+      .setStyle(ButtonStyle.Secondary);
 
     await interaction.reply({
-      components: [new ActionRowBuilder().addComponents([docsButton, setRoleButton, setChannelButton])],
+      components: [
+        new ActionRowBuilder().addComponents([
+          docsButton,
+          setRoleButton,
+          setChannelButton,
+        ]),
+      ],
+      embeds: [embed],
     });
   },
 };
