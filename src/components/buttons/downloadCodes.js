e1b9024a6f5473ecde9cd9768c2bc409852d5675
muffafa <mm.savar@gmail.com>
2023-04-30 14:02:04 +0300

    feat: moderasyondaki tebrikler embedi oluşturuldu

---
+++ src/components/buttons/downloadCodes.js
@@ -0,0 +1,13 @@
+module.exports = {
+    data: {
+      name: "downloadCodes",
+    },
+    // eslint-disable-next-line no-unused-vars
+    async execute(interaction, client) {
+        // TODO: DUMMY TEXT WILL BE OVERWRITTEN
+      await interaction.reply({ 
+        content: `Kodlar indirildi!`,
+      });
+    },
+  };
+  
--- src/components/modals/createCodesScreen.js
+++ src/components/modals/createCodesScreen.js
@@ -1,17 +1,56 @@
-module.exports = {
-    data: {
-      name: "createCodesScreen",
-    },
-    async execute(interaction, client, roleId) {
-      const codeNumber = interaction.fields.getTextInputValue("codeNumberInput");
-      // TODO: create random codes by code number input
-      // TODO: check if code number input is valid (is it a number?)
-      // TODO: convert string input to number 
-      // TODO: save codes to database with roleCode
+const {
+  ButtonBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+  Colors,
+} = require("discord.js");
 
-      await interaction.reply({
-        content: `Oluştrulmak istenen kod sayısı: ${codeNumber}\nAtanacak rol:${roleId}`,
-      });
-    },
-  };
+module.exports = {
+  data: {
+    name: "createCodesScreen",
+  },
   
+  // eslint-disable-next-line no-unused-vars
+  async execute(interaction, client, roleId) {
+    //const codeNumber = interaction.fields.getTextInputValue("codeNumberInput");
+    // TODO: create random codes by code number input
+    // TODO: check if code number input is valid (is it a number?)
+    // TODO: convert string input to number
+    // TODO: save codes to database with roleCode
+
+    // await interaction.reply({
+    //   content: `Oluştrulmak istenen kod sayısı: ${codeNumber}\nAtanacak rol:${roleId}`,
+    // });
+
+    const embed = new EmbedBuilder()
+      .setColor(Colors.Blue)
+      .setImage(client.user.displayAvatarURL()) // TODO: Resim figmadaki resimle değiştirilecek
+      .setThumbnail(interaction.user.displayAvatarURL())
+      .setAuthor({
+        url: "https://github.com/Kodluyoruz/discord-register-bot",
+        iconURL: client.user.displayAvatarURL(),
+        name: `Kodluyoruz Kayıt Botu`,
+      })
+      .setURL("https://github.com/Kodluyoruz/discord-register-bot")
+      .addFields([
+        {
+          name: `TEBRİKLER KODLAR OLUŞTURULDU`,
+          value: `kodlar...`, // TODO: codes will be displayed
+          inline: false,
+        },
+      ]);
+
+    const downloadsCodesButton = new ButtonBuilder() // TODO: codes can be downloaded here
+      .setCustomId("downloadCodes")
+      .setLabel("Kodları İndir")
+      .setStyle(ButtonStyle.Success);
+
+    await interaction.reply({
+      components: [
+        new ActionRowBuilder().addComponents([downloadsCodesButton]),
+      ],
+      embeds: [embed],
+    });
+  },
+};
