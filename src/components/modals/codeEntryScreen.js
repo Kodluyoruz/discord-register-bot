7a659e7a70d5baa48c0392b76cdcf6d016696e7d
muffafa <mm.savar@gmail.com>
2023-04-30 14:53:47 +0300

    feat: kayıt ol frontendi oluşturuldu

--- src/components/modals/codeEntryScreen.js
+++ src/components/modals/codeEntryScreen.js
@@ -1,15 +1,39 @@
+const {
+  EmbedBuilder,
+  Colors,
+} = require("discord.js");
+
 module.exports = {
   data: {
     name: "codeEntryScreen",
   },
-  async execute(interaction) {
-    let codeInput = interaction.fields.getTextInputValue("codeInput");
+  async execute(interaction, client) {
+    // TODO: CHECK IT FOR IS THIS REGISTRATION CODE IS CORRECT
+    const codeInput = interaction.fields.getTextInputValue("codeInput");
     // let code = await client.db.get("code");
-    let code = "123";
+    const code = "123";
 
     if (codeInput == code) {
+      const embed = new EmbedBuilder()
+        .setColor(Colors.Blue)
+        .setImage(client.user.displayAvatarURL()) // TODO: Resim figmadaki resimle değiştirilecek
+        .setThumbnail(interaction.user.displayAvatarURL())
+        .setAuthor({
+          url: "https://github.com/Kodluyoruz/discord-register-bot",
+          iconURL: client.user.displayAvatarURL(),
+          name: `Kodluyoruz Kayıt Botu`,
+        })
+        .setURL("https://github.com/Kodluyoruz/discord-register-bot")
+        .addFields([
+          {
+            name: `TEBRİKLER`,
+            value: `@rol-adi rol başarı ile tanımlandı. Bu rolde ......`, // TODO: user role should be shown here
+            inline: false,
+          },
+        ]);
+
       await interaction.reply({
-        content: `Kayıt kodunuz doğru. Hoşgeldiniz. ${interaction.user.username}, Kayıt kodunuz:${codeInput}`,
+        embeds: [embed],
       });
       return;
     } else {
