a8a7e5227f133990ed1564686881426a379e7650
muffafa <mm.savar@gmail.com>
2023-05-01 00:38:45 +0300

    docs: todo daha açıklayıcı hale getirildi

--- src/components/buttons/downloadCodes.js
+++ src/components/buttons/downloadCodes.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     // eslint-disable-next-line no-unused-vars
     async execute(interaction, client) {
-        // TODO: DUMMY TEXT WILL BE OVERWRITTEN
+        // TODO: DUMMY TEXT WILL BE OVERWRITTEN AND CODES MUST BE DOWNLOADED
       await interaction.reply({ 
         content: `Kodlar indirildi!`,
       });
