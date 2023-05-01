97728ca515717374021dc6a08730002e0f65d896
Bilgehan Zeki Özaytaç <zekiozaytac@hotmail.com.tr>
2023-05-01 11:57:32 +0300

    update: komutların rest api ile güncellenmesi işlemi kaldırıldı

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,5 +1,3 @@
-import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
 import fs from "fs";
 
 export default (client) => {
@@ -19,21 +17,6 @@ export default (client) => {
         commandArray.push(command.data.toJSON());
         logger.info(`Komut: ${command.data.name} yüklendi`);
       }
-    }
-
-    const clientId = process.env.clientId;
-    const guildId = process.env.guildId;
-    const rest = new REST({ version: "9" }).setToken(process.env.token);
-    try {
-      logger.info("Uygulama (/) komutları yükleniyor.");
-
-      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-        body: client.commandArray,
-      });
-
-      logger.info("Uygulama (/) komutları başarıyla yüklendi.");
-    } catch (error) {
-      logger.error(error);
     }
   };
 };
