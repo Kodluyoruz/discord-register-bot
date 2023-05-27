c9482bde9d10cf3f5a764f9088f26978cb3985fc
Bilgehan Zeki Özaytaç <zekiozaytac@hotmail.com.tr>
2023-05-28 00:54:41 +0300

    update: Ornek log kanalı mesajı eklendi

--- src/components/modals/codeEntryScreen.js
+++ src/components/modals/codeEntryScreen.js
@@ -1,5 +1,6 @@
 import Code from "../../schemas/code.js";
 import { Colors, EmbedBuilder } from "discord.js";
+import Setting from "../../schemas/setting.js";
 
 export default {
   data: {
@@ -56,6 +57,41 @@ export default {
       await interaction.editReply({
         embeds: [embed],
       });
+
+      const { guild } = interaction;
+
+      Setting.getValueByKey(guild.id, "Channel:Log").then(async (setting) => {
+        if (setting) {
+          const channel = guild.channels.cache.find(
+            (channel) => channel.id === setting.value
+          );
+
+          // TODO: Log kanalına gönderilen embedler güncellencek
+          const logEmbed = new EmbedBuilder()
+            .setColor(Colors.Blue)
+            .setImage(client.user.displayAvatarURL()) // TODO: Resim figmadaki resimle değiştirilecek
+            .setThumbnail(interaction.user.displayAvatarURL())
+            .setAuthor({
+              url: "https://github.com/Kodluyoruz/discord-register-bot",
+              iconURL: client.user.displayAvatarURL(),
+              name: `Kodluyoruz Kayıt Botu`,
+            })
+            .setURL("https://github.com/Kodluyoruz/discord-register-bot")
+            .addFields([
+              {
+                name: `Güncellenen Rol ${nameInput}`,
+                value: `${roles
+                  .map((role) => `<@&${role.id}>`)
+                  .join(", ")} rolleri başarı ile tanımlandı. Bu rolde ......`, // TODO: user role should be shown here
+                inline: false,
+              },
+            ]);
+          await channel.send({
+            embeds: [embed],
+          });
+        }
+      });
+
     } else if (codeEntry && codeEntry.userId) {
       await interaction.editReply({
         content: `Bu kod kullanılmış.`,
