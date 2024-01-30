9c5c9a588270dbaf9cab5104cf90d57f21f332fd
Timur Sevimli <svmlitimur@gmail.com>
2024-01-30 15:48:24 +0300

    Fix import and update style

--- src/functions/handlers/handleEvents.js
+++ src/functions/handlers/handleEvents.js
@@ -1,8 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
-import mongoose from "mongoose";
+import * as mongo from "mongoose";
 
 const EVENTS_PATH = './src/events/';
 
@@ -10,18 +8,18 @@ const isJs = (file = "") => file.endsWith(".js");
 const isDir = (dir) => fs.statSync(path.join(EVENTS_PATH, dir)).isDirectory();
 
 export default (client) => {
-  const methods = { client, mongo: mongoose };
-  client.handleEvents = () => {
+  const methods = { client, mongo };
+  client.handleEvents = async () => {
     const eventFolders = fs.readdirSync(EVENTS_PATH).filter(isDir);
     for (const folder of eventFolders) {
       const eventPath = path.join(EVENTS_PATH, folder);
       const eventFiles = fs.readdirSync(eventPath).filter(isJs);
       const method = methods[folder];
       for (const file of eventFiles) {
-        const eventFile = path.join(eventPath, file);
-        const event = require(eventFile).default;
+        const eventFile = path.join('../', eventPath, file);
+        const event = (await import(eventFile)).default;
         const listener = event.once ? method.once : method.on;
-        listener(event.name, (...args) => event.execute(...args, client))
+        listener(event.name, (...args) => event.execute(...args, client));
       }
     }
   };
